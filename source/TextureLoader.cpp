ac0096a276b90c0a59f5fc749fdf2938a8d80735
zz <zhuguang@ejoy.com>
2019-03-25 07:12:30 +0800

    [ADDED] rgb32f

--- source/TextureLoader.cpp
+++ source/TextureLoader.cpp
@@ -49,6 +49,9 @@ ur::TexturePtr TextureLoader::LoadFromFile(const char* filepath)
 	case GPF_BGR_EXT:
 		tf = ur::TEXTURE_BGR_EXT;
 		break;
+    case GPF_RGB32F:
+        tf = ur::TEXTURE_RGB32F;
+        break;
 	case GPF_COMPRESSED_RGBA_S3TC_DXT1_EXT:
 		tf = ur::TEXTURE_COMPRESSED_RGBA_S3TC_DXT1_EXT;
 		break;
