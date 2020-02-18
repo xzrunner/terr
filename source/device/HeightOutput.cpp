0f8a9ef502b628a4d305d6598ad08ebb67b29316
zz <xzrunner@gmail.com>
2020-02-19 06:23:55 +0800

    [ADDED] config to output rgb heightmap

--- source/device/HeightOutput.cpp
+++ source/device/HeightOutput.cpp
@@ -5,6 +5,8 @@
 #include <gimg_export.h>
 #include <gimg_typedef.h>
 
+//#define OUTPUT_SINGLE_CHANNEL
+
 namespace wm
 {
 namespace device
@@ -22,11 +24,22 @@ void HeightOutput::Execute()
 
     auto& vals = prev_hf->GetValues();
     assert(vals.size() == w * h);
+#ifdef OUTPUT_SINGLE_CHANNEL
     std::vector<uint8_t> pixels(vals.size());
     for (size_t i = 0, n = vals.size(); i < n; ++i) {
         pixels[i] = static_cast<uint8_t>(vals[i] * 255.0f);
     }
     gimg_export(m_filepath.c_str(), pixels.data(), w, h, GPF_LUMINANCE, 1);
+#else
+    std::vector<uint8_t> pixels(vals.size() * 3);
+    for (size_t i = 0, n = vals.size(); i < n; ++i) {
+        auto v = static_cast<uint8_t>(vals[i] * 255.0f);
+        for (size_t j = 0; j < 3; ++j) {
+            pixels[i * 3 + j] = v;
+        }
+    }
+    gimg_export(m_filepath.c_str(), pixels.data(), w, h, GPF_RGB, 1);
+#endif // OUTPUT_SINGLE_CHANNEL
 }
 
 }
