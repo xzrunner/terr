2963c61bd28770827524f91266eb1b877840a0f2
zz <xzrunner@gmail.com>
2020-02-26 07:57:37 +0800

    [FIXED] reset dirty

--- source/HeightField.cpp
+++ source/HeightField.cpp
@@ -19,7 +19,7 @@ ur::TexturePtr HeightField::GetHeightmap()
     if (!m_heightmap || m_dirty) {
         auto& rc = ur::Blackboard::Instance()->GetRenderContext();
         m_heightmap = TextureBaker::GenHeightMap(*this, rc);
-        m_dirty = true;
+        m_dirty = false;
     }
     return m_heightmap;
 }
